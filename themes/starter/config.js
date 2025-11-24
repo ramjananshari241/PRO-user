9e2dc34b130cf44943278734d45fbe25717c172c
kprimr9 <kprimr9@gmail.com>
2025-11-24 18:06:59 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -11,7 +11,7 @@ const CONFIG = {
   // 英雄区块导航
   STARTER_HERO_ENABLE: true, // 开启英雄区
   STARTER_HERO_TITLE_1: 'PRO+', // 英雄区文字
-  STARTER_HERO_TITLE_2: '一站式资源仓库 永不丢失!', // 英雄区文字
+  STARTER_HERO_TITLE_2: '你的一站式收藏夹 资源永不丢失!', // 英雄区文字
   // 英雄区两个按钮，如果TEXT留空则隐藏按钮
   STARTER_HERO_BUTTON_1_TEXT: '会员登录', // 英雄区按钮
   STARTER_HERO_BUTTON_1_URL:
