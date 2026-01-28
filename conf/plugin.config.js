0df5eb7470fe2215aaebcf3b26e1fc293d555cd5
kprimr9 <kprimr9@gmail.com>
2026-01-28 18:34:07 +0800

    Update plugin.config.js

--- conf/plugin.config.js
+++ conf/plugin.config.js
@@ -1,18 +1,14 @@
 /**
- * 一些插件
+ * 一些插件配置
  */
 module.exports = {
-  // 网站全文搜索
-  // ALGOLIA_APP_ID: process.env.NEXT_PUBLIC_ALGOLIA_APP_ID || null, // 在这里查看 https://dashboard.algolia.com/account/api-keys/
-  // ALGOLIA_ADMIN_APP_KEY: process.env.ALGOLIA_ADMIN_APP_KEY || null, // 管理后台的KEY，不要暴露在代码中，在这里查看 https://dashboard.algolia.com/account/api-keys/
-  ALGOLIA_APP_ID: process.env.NEXT_PUBLIC_ALGOLIA_APP_ID || '8Q0603I0T3', // 在这里查看 https://dashboard.algolia.com/account/api-keys/
-  ALGOLIA_ADMIN_APP_KEY: process.env.ALGOLIA_ADMIN_APP_KEY || '7903e22ec0eec373bf953ccde408544b', 
-  ALGOLIA_SEARCH_ONLY_APP_KEY:
-    process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_ONLY_APP_KEY || null, // 客户端搜索用的KEY
-  ALGOLIA_INDEX: process.env.NEXT_PUBLIC_ALGOLIA_INDEX || null, // 在Algolia中创建一个index用作数据库
+  // 网站全文搜索 - 已禁用以解决部署报错
+  ALGOLIA_APP_ID: null, 
+  ALGOLIA_ADMIN_APP_KEY: null, 
+  ALGOLIA_SEARCH_ONLY_APP_KEY: null, 
+  ALGOLIA_INDEX: null, 
 
   // AI 文章摘要生成
-
   AI_SUMMARY_API: process.env.AI_SUMMARY_API || '',
   AI_SUMMARY_KEY: process.env.AI_SUMMARY_KEY || '',
   AI_SUMMARY_CACHE_TIME: process.env.AI_SUMMARY_CACHE_TIME || 1800, // 缓存时间，单位秒
@@ -29,6 +25,6 @@ module.exports = {
   TianliGPT_KEY: process.env.NEXT_PUBLIC_TIANLI_GPT_KEY || '',
 
   // 邮件
-  MAILCHIMP_LIST_ID: process.env.MAILCHIMP_LIST_ID || null, // 开启mailichimp邮件订阅 客户列表ID ，具体使用方法参阅文档
+  MAILCHIMP_LIST_ID: process.env.MAILCHIMP_LIST_ID || null, // 开启mailichimp邮件订阅 客户列表ID
   MAILCHIMP_API_KEY: process.env.MAILCHIMP_API_KEY || null // 开启mailichimp邮件订阅 APIkey
 }
